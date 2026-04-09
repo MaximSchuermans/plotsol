78472ec9cd31169908010a20edda4371dd4ea2b6
Maxim Schuermans <schuermansmaxim1@gmail.com>
2026-04-09 22:58:14 +0200

    forntend login page and backend health api endpoint

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
